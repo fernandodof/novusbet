d6c269fab1870427e3f8c57e3158365fa412a116
fernandodof <fernandodof@gmail.com>
2016-03-09 22:03:08 -0300

    starting to use simple dashboard side menu and ui angular bootstrap

--- public/app/app.modules.js
+++ public/app/app.modules.js
@@ -1,5 +1,6 @@
 var app = angular.module('novusbet', [
     'ui.router',
+    'ui.bootstrap',
     'jcs-autoValidate',
     
     //Routes config
--- public/app/components/dashboard/dashboardController.js
+++ public/app/components/dashboard/dashboardController.js
@@ -1,3 +1,25 @@
-angular.module('novusbet.components.dashboard.DashboardController',[]).controller('DashboardController',function(){
+angular.module('novusbet.components.dashboard.DashboardController',[]).controller('DashboardController',function($scope){
+    ctrl = this;
+    
+    ctrl.sidebarToggle = true;
+    
+    var mobileView = 992;
+
+    ctrl.getWidth = function() {
+        return window.innerWidth;
+    };
+
+    $scope.$watch(ctrl.getWidth, function(newValue) {
+        if (newValue >= mobileView) {
+            ctrl.sidebarToggle = true;
+        } else {
+           ctrl.sidebarToggle = false;
+        }
+
+    });
+
+    window.onresize = function() {
+        $scope.$apply();
+    };
     
 });
